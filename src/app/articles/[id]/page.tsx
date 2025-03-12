03bc728ebddfa441c9fa10bfee323e110875e06c
Marc Le Moigne <37547382+qQsss777@users.noreply.github.com>
2025-03-12 14:45:01 +0100

    feat: suppression des icones et création des routes dynamiques
    Suppression des icones et création des routes dynamiques

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,19 +1,53 @@
 import styles from "../../page.module.css";
+import { promises as fs } from "fs";
+import { notFound } from "next/navigation";
+import type { Metadata, ResolvingMetadata } from "next";
 
-export function generateStaticParams() {
-  return [{ id: '1' }, { id: '2' }, { id: '3' }]
+type Props = {
+  params: Promise<{ id: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+const articlesPath = "/public/articles";
+
+export async function generateStaticParams(): Promise<{ id: string }[]> {
+  try {
+    const folders = await fs.readdir(process.cwd() + articlesPath);
+    return folders.map((folder) => {
+      return { id: folder };
+    });
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const { id } = await params;
+  const { title } = await parent;
+  return {
+    title: `${title?.absolute} - ${id}`,
+  };
 }
 
 export default async function Page({
   params,
 }: {
-  params: Promise<{ id: string }>
+  params: Promise<{ id: string }>;
 }) {
-  const { id } = await params
-  console.log(id)
-  return (
-    <div className={styles.page}>
-      {id}
-    </div>
-  );
+  try {
+    const { id } = await params;
+    const path = process.cwd() + articlesPath + "/" + id + "/page.md";
+    const buffer = await fs.readFile(path, { encoding: "utf-8" });
+    const nextLineCharacter = process.platform === "win32" ? "\r\n" : "\n";
+    const lines = buffer.split(nextLineCharacter);
+    console.log(lines);
+    return <div className={styles.page}>{buffer}</div>;
+  } catch (error) {
+    console.error(error);
+    notFound();
+  }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import styles from "./page.module.css";
 
 export default async function Home() {
-  return (
-    <div className={styles.page}>
-      Hello World
-    </div>
-  );
+  return <div className={styles.page}>Hello World</div>;
 }
