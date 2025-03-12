1f36f3bbab1a03c3fad163727ef1847a25086961
Marc Le Moigne <37547382+qQsss777@users.noreply.github.com>
2025-03-12 17:37:37 +0100

    build: ajout des basepaths pour le build des pages
    Ajout des basepaths pour le build des pages

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   distDir: "dist",
+  basePath: "/next-blog",
+  assetPrefix: "/next-blog",
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
