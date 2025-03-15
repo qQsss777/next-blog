a8a01d7a18f4faf083b3fb6387dfaa34089fb1a6
Marc Le Moigne <37547382+qQsss777@users.noreply.github.com>
2025-03-15 13:48:34 +0100

    feat(markdownparser): ajout du markdown parser pour les titres et paragraphes
    Ajout du markdown parser pour les titres et paragraphes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+import { notFound } from "next/navigation";
+import ArticleContainer from "./components/articleContainer";
+import { homePath } from "./constants";
+import { getFileAsString } from "./lib/directoryUtils";
+import MarkdownParser from "./lib/markdownParser";
+
 export default async function Home() {
-  return <div></div>;
+  try {
+    const path = process.cwd() + homePath + "/page.md";
+    const rawData = await getFileAsString(path);
+    const nextLineCharacter = process.platform === "win32" ? "\r\n" : "\n";
+    const markdownParser = new MarkdownParser({
+      nextLineCharacter,
+      rawData,
+    });
+    const data = markdownParser.getParsedData();
+    return (
+      <ArticleContainer>
+        <div>{data.toString()}</div>
+        <div>{data.toString()}</div>
+      </ArticleContainer>
+    );
+  } catch (error) {
+    console.error(error);
+    notFound();
+  }
 }
